92a1d9fe16c9cd2f7996c0507fcae7549f0a409c
Fran Mosteiro <framosval@gmail.com>
2013-04-22 20:52:34 +0200

    Week 4. Exmaples. List && IntSets (covariance, contravariance, invariance)

--- Progfun-VideoClass-Examples/src/week4/List.scala
+++ Progfun-VideoClass-Examples/src/week4/List.scala
@@ -2,27 +2,26 @@ package week4
 
 import java.util.NoSuchElementException
 
-trait List[T] {
+trait List[+T] {
 	def isEmpty: Boolean
 	def head: T
 	def tail: List[T]
+	// Convert inmutable in mutable: covariant type T occurs in contravariant position in type T of value  elem
+	//def prepend(elem: T): List[T] = new Cons(elem, this) // _> violates Liskov
+	def prepend [U >: T](elem: U): List[U] = new Cons(elem, this)
 }
 
 class Cons[T] (val head: T, val tail: List[T]) extends List[T] {
   def isEmpty = false
 }
 
-class Nil[T] extends List[T] {
+object Nil extends List[Nothing] {
   def isEmpty: Boolean = true
   def head: Nothing = throw new NoSuchElementException("Nil.head")
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
 }
 
-object List {
-  //List(1,2) = List.apply(1,2)
-  def apply[T](x1:T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
-  //List(1) = List.apply(1)
-  def apply[T](x1:T): List[T] = new Cons(x1, new Nil)
-  //List()
-  def apply[T]: List[T] = new Nil
-}
+object Test {
+  val x: List[String] = Nil
+  def f(xs: List[NonEmpty], x: Empty) = xs prepend x
+ }
